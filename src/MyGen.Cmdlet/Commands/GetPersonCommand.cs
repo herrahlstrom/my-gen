85cdc79019b86891ce44b2a0cf8f29747d785ded
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-01-08 11:46:55 +0100

    Add basic command for Get-MyGenPerson

---
+++ src/MyGen.Cmdlet/Commands/GetPersonCommand.cs
@@ -0,0 +1,49 @@
+﻿using MyGen.Cmdlet.Models;
+using MyGen.Data;
+using MyGen.Data.Models;
+using System.Management.Automation;
+
+[Cmdlet(VerbsCommon.Get, "MyGenPerson")]
+public class GetPersonCommand : Cmdlet
+{
+   [Parameter]
+   public Guid Id { get; set; } = Guid.Empty;
+
+   [Parameter]
+   public string Filter { get; set; } = "";
+
+   private CrudableRepository _repository;
+
+   public GetPersonCommand()
+   {
+      var fs = new FileSystem("C:\\Users\\marti\\source\\repos\\my-gen-data");
+      _repository = new CrudableRepository(fs);
+      _repository.Load();
+   }
+
+   protected override void ProcessRecord()
+   {
+      if (Id != Guid.Empty)
+      {
+         var result = _repository.GetEntity<Person>(Id);
+         WriteObject(PSPerson.Get(result));
+      }
+      else
+      {
+         IEnumerable<Person> result = _repository.GetEntities<Person>();
+
+         if (!string.IsNullOrEmpty(Filter))
+         {
+            foreach (var word in Filter.ToLower().Split())
+            {
+               result = result.Where(x => x.Firstname.Contains(word, StringComparison.OrdinalIgnoreCase) ||
+                                          x.Lastname.Contains(word, StringComparison.OrdinalIgnoreCase) ||
+                                          x.Profession.Contains(word, StringComparison.OrdinalIgnoreCase) ||
+                                          x.Notes.Contains(word, StringComparison.OrdinalIgnoreCase));
+            }
+         }
+
+         WriteObject(result.Select(PSPerson.Get), true);
+      }
+   }
+}
---
+++ src/MyGen.Cmdlet/Models/PSPerson.cs
@@ -0,0 +1,26 @@
+﻿using MyGen.Data.Models;
+
+namespace MyGen.Cmdlet.Models;
+
+public class PSPerson
+{
+   public required Guid Id { get; set; }
+   public required string Firstname { get; set; }
+   public required string Lastname { get; set; }
+   public required string Sex { get; set; }
+   public required string Profession { get; set; }
+   public required string Notes { get; set; }
+
+   internal static PSPerson Get(Person entity)
+   {
+      return new PSPerson()
+      {
+         Id = entity.Id,
+         Firstname = entity.Firstname,
+         Lastname = entity.Lastname,
+         Sex = entity.Sex,
+         Profession = entity.Profession,
+         Notes = entity.Notes
+      };
+   }
+}
