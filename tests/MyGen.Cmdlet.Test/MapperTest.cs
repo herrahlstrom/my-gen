5afff37772f94625be015893632e8bc81e47e886
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-01-08 23:40:07 +0100

    Add mapper test

---
+++ tests/MyGen.Cmdlet.Test/MapperTest.cs
@@ -0,0 +1,27 @@
+﻿using AutoBogus;
+using FluentAssertions;
+using MyGen.Api.Shared.Models;
+
+namespace MyGen.Cmdlet.Test;
+
+[TestClass]
+public class MapperTest
+{
+   Mapper _mapper = null!;
+
+   [TestInitialize]
+   public void Initialize()
+   {
+      _mapper = new Mapper();
+   }
+
+   [TestMethod]
+   public void PersonDto_To_PSPerson()
+   {
+      PersonDto dto = new AutoFaker<PersonDto>().Generate();
+
+      var psModel = _mapper.ToPsModel(dto);
+
+      psModel.Should().BeEquivalentTo(dto);
+   }
+}
