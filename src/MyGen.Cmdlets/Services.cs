78fa365425208664f0749bda3c066a6528d31f89
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-01-10 19:37:49 +0100

    Implement GetPersonCommandTest and implove Services

--- src/MyGen.Cmdlets/Services.cs
+++ src/MyGen.Cmdlets/Services.cs
@@ -5,35 +5,53 @@ namespace MyGen.Cmdlets;
 
 internal class Services : IServiceProvider, IDisposable
 {
-   private static Services? _instance = null;
-   private readonly ServiceProvider _sp;
+   private static readonly List<ServiceProvider> _serviceProviders = new();
+   private readonly ServiceProvider _serviceProvider;
 
-   private Services(ServiceProvider sp)
+   static Services()
    {
-      _sp = sp;
+      Instance = new();
    }
 
-   public static Services Instance => _instance ??= Create();
-
-   public static Services Create()
+   public Services(Action<ServiceCollection> scBuilder)
    {
       var sc = new ServiceCollection();
+      scBuilder.Invoke(sc);
+      _serviceProvider = sc.BuildServiceProvider();
 
-      sc.AddHttpClient<ApiClient>(config => { config.BaseAddress = new Uri("http://localhost:5022"); });
-      sc.AddTransient<IApiClient, ApiClient>();
-
-      sc.AddTransient<Mapper>();
-
-      return new Services(sc.BuildServiceProvider());
+      _serviceProviders.Add(_serviceProvider);
    }
+
+   private Services() : this(CreateDefaultServiceCollection)
+   {
+   }
+
+   public static Services Instance { get; }
 
    public void Dispose()
    {
-      _sp.Dispose();
+      _serviceProviders.Remove(_serviceProvider);
+      _serviceProvider.Dispose();
    }
 
    public object? GetService(Type serviceType)
    {
-      return _sp.GetService(serviceType);
+      for (int i = _serviceProviders.Count - 1; i >= 0; i--)
+      {
+         if (_serviceProviders[i].GetService(serviceType) is { } s)
+         {
+            return s;
+         }
+      }
+
+      return default;
+   }
+
+   private static void CreateDefaultServiceCollection(ServiceCollection sc)
+   {
+      sc.AddHttpClient<ApiClient>(config => { config.BaseAddress = new Uri("http://localhost:5022"); });
+      sc.AddTransient<IApiClient, ApiClient>();
+
+      sc.AddTransient<Mapper>();
    }
 }
---
+++ tests/MyGen.Cmdlet.Test/Helpers/PowerShellExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+using System.Reflection;
+
+namespace MyGen.Cmdlet.Test.Helpers;
+
+internal static class PowerShellExtensions
+{
+    public static PowerShell AddCommand<TCommand>(this PowerShell ps) where TCommand : System.Management.Automation.Cmdlet
+    {
+        var cmdletAttr = typeof(TCommand).GetCustomAttribute<CmdletAttribute>()!;
+        string name = $"{cmdletAttr.VerbName}-{cmdletAttr.NounName}";
+        ps.AddCommand(new CmdletInfo(name, typeof(TCommand)));
+        return ps;
+    }
+}
