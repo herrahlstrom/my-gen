dcfc39639b0f4a00223e4536a88e263234416476
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-01-10 22:45:41 +0100

    Update import module script

--- import-modules.ps1
+++ import-modules.ps1
@@ -1,3 +1,10 @@
+if (Get-Module -Name MyGen.Cmdlet) {
+    Write-Host "Remove-Module MyGen.Cmdlets"
+    Remove-Module MyGen.Cmdlets
+} 
+
 dotnet publish -c Release .\src\MyGen.Cmdlets\MyGen.Cmdlets.csproj
 
-Import-Module .\src\MyGen.Cmdlets\bin\Release\net7.0\publish\MyGen.Cmdlet.dll
+Write-Host "Import-Module MyGen.Cmdlets"
+$fullpath = Resolve-Path src\MyGen.Cmdlets\bin\Release\net7.0\publish\MyGen.Cmdlets.dll
+Import-Module $fullpath.ToString()
