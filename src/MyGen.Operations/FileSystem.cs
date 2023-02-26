65ce3a183294245f96ab1f89c1f8dd36cd703bee
Martin Ahlström <martin.ahlstrom@outlook.com>
2023-02-26 11:33:27 +0100

    (re)create MyGen.Operations

---
+++ src/MyGen.Operations/FileSystem.cs
@@ -0,0 +1,38 @@
+﻿using MyGen.Data;
+
+namespace MyGen.Operations;
+
+public class FileSystem : IFileSystem
+{
+   private readonly string _path;
+
+   public FileSystem(string path)
+   {
+      _path = path;
+   }
+
+   public void DeleteFile(string filename)
+   {
+      File.Delete(Path.Combine(_path, filename));
+   }
+
+   public ICollection<string> GetFiles()
+   {
+      return Directory.GetFiles(_path);
+   }
+
+   public ICollection<string> GetFiles(string extension)
+   {
+      return Directory.GetFiles(_path, $"*{extension}");
+   }
+
+   public Stream OpenForRead(string filename)
+   {
+      return new FileStream(Path.Combine(_path, filename), FileMode.Open);
+   }
+
+   public Stream OpenForWrite(string filename)
+   {
+      return new FileStream(Path.Combine(_path, filename), FileMode.Create);
+   }
+}
